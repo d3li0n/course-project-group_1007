e8f0de9c4049f861a9e0474e532e4fa96f7c91b2
Dima Zhuravel <dmitrozhyravel@gmail.com>
2021-03-25 21:59:58 -0700

    Updated projects_functions.py for Task3 Milestone 3. Executed new method to save formatted data in data/processed folder

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -29,7 +29,7 @@ def load_and_process(result):
               'LSOA_of_Accident_Location', '1st_Road_Class', 
               '1st_Road_Number', 'Special_Conditions_at_Site',
               'Light_Conditions', '2nd_Road_Class', '2nd_Road_Number', 'Junction_Detail', 
-              'Local_Authority_(District)', 'Local_Authority_(Highway)', 'Longitude', 'Latitude'], axis=1)
+              'Local_Authority_(District)', 'Local_Authority_(Highway)'], axis=1)
         .reset_index(drop=True)
         .rename(columns={"Weather_Conditions": "Weather_Type", "Did_Police_Officer_Attend_Scene_of_Accident": "Police_Presense"})
         .replace({'Police_Presense': {'Yes': True, 'No': False}})
@@ -39,8 +39,12 @@ def load_and_process(result):
   )
   df2['Date'] = pd.to_datetime(df2['Date'], format='%d/%m/%Y')
 
-  ''' Testing environment'''
-  #print(df2.dtypes)
-  #print(df2[])
-
   return df2
+
+def process_data(data):
+  SPLIT_FILE_NUMBER = 12 # Number of files we want to split
+  for i, merged_df in enumerate(np.array_split(data, SPLIT_FILE_NUMBER)): # Figuring out how we want to split data size between SPLIT_FILE_NUMBER
+    print('🚨 Started to create formatted .csv file [{}/{}]\n'.format(i + 1, SPLIT_FILE_NUMBER)) # Displaying/testing if everything works properly
+    with open('{}/out{}.csv'.format(os.path.join('..', '..', 'data','processed'), i + 1), "w") as new_file: # Creating file
+      new_file.write(merged_df.to_csv()) # Writing data to new file
+  print("👍 Finished creating formatted .csv files") # Showing result to confirm 
